907de771df1d786f7f91e5c66197b59e632ee98f
Nicolas Lee <nickleebr@gmail.com>
2025-06-07 19:50:28 -0300

    Front: Refactor fxml & controllers location

--- src/pokeclicker/Main.java
+++ src/pokeclicker/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("initialScene.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("views/initialScene.fxml"));
             Scene scene = new Scene(root);
 
 
---
+++ src/pokeclicker/controllers/InitialController.java
@@ -0,0 +1,70 @@
+package pokeclicker.controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.rmi.UnexpectedException;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import pokeclicker.controllers.ProfileController;
+import pokeclicker.manager.UserManager;
+
+public class InitialController implements Initializable{
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    @FXML
+    Label titleLabel;
+    @FXML
+    Label exceptionLabel;
+    @FXML
+    TextField inputField;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) 
+    {
+        titleLabel.setText("Welcome, trainer!\n\nTo begin your journey, enter your name and press \"Start\"");
+        exceptionLabel.setText("");
+    }
+
+    public void getUsername(ProfileController profileController) throws UnexpectedException
+    {
+        String username = inputField.getText().trim();
+        if (username == "")
+        {
+            exceptionLabel.setText("Empty username!");
+            throw new UnexpectedException("Empty username!");
+        }
+
+        UserManager.createUser(username);
+        profileController.displayName(username);       
+    }
+
+    public void switchToProfile(ActionEvent event) throws IOException
+    {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/profileScene.fxml"));
+        Parent root = loader.load();
+        ProfileController profileController = loader.getController();
+
+        getUsername(profileController);
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
