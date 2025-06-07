5bc62d368fdbbf8902e74e8124cbaf568b20a37a
Nicolas Lee <nickleebr@gmail.com>
2025-06-07 12:47:43 -0300

    Front: Icon and title to window

--- src/pokeclicker/Main.java
+++ src/pokeclicker/Main.java
@@ -4,14 +4,36 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/pokeclicker/pokeclicker.fxml"));
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("initialScene.fxml"));
+            Scene scene = new Scene(root);
+
+
+            String cssPath = this.getClass().getResource("initialScene.css").toExternalForm();
+            scene.getStylesheets().add(cssPath);
+
+            // Set window icon
+            Image windowIcon = new Image("/img/windowIcon.png");
+            stage.getIcons().add(windowIcon);
+
+            // Set title
+            stage.setTitle("PokeClicker - Click and evolve your Pokemons!");
+
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            throw e;
+        }
+        
+        
     }
 
     public static void main(String[] args) {
